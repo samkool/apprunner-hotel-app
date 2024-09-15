e11c3f39245a1e8416218cb763e03b650f5e4747
Sam Kool <samkool@amazon.com>
2024-09-15 21:31:35 +0200

    remove secrets from parameters

--- routes/params.js
+++ routes/params.js
@@ -24,9 +24,7 @@ configPromise.then((config) => {
     console.log('Config loaded:', config);
     /* display room list */
     router.get('/', function(req, res, next) {
-      var secret = JSON.parse(config.secret.db_secret);
-      secret.password = "Shhhh! It's a secret"
-      res.render('param-list', { menuTitle: config.app.hotel_name, infraParams: config.infra, appParams: config.app, secretParams: secret });
+        res.render('param-list', { menuTitle: config.app.hotel_name, infraParams: config.infra, appParams: config.app});
     });
 }).catch((error) => {
   console.error('Error loading config:', error);
